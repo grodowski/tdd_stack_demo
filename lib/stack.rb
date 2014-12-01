247cabecb5b3f85e161570e4a0764b74a7cddbc4
Jan Grodowski <jgrodowski@gmail.com>
2014-12-01 19:16:26 +0100

    stack example ready

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,2 +1,17 @@
-class Stack
+class Stack  
+  def initialize
+    @elements = []
+  end
+  
+  def size 
+    @elements.size
+  end
+  
+  def push(element)
+    @elements << element
+  end
+  
+  def pop
+    @elements.pop
+  end
 end
--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,9 +1,36 @@
-require 'rubygems'
-require 'bundler/setup'
 require 'stack'
 
-RSpec.describe Stack do 
-  it 'is true' do 
-    expect(true).to eq true
+describe Stack do 
+  subject { Stack.new } 
+  let(:element) { :element }
+  
+  it 'has size eq 0' do 
+    expect(subject.size).to eq 0
+  end
+  
+  context 'after push' do 
+    before do
+      subject.push element
+    end
+    
+    it 'has size 1 after push' do 
+      expect(subject.size).to eq 1
+    end
+  
+    it 'has size 0 after pop' do 
+      subject.pop
+      expect(subject.size).to eq 0
+    end
+  
+    it 'pop returns last element' do 
+      expect(subject.pop).to eq element
+    end
+  end
+  
+  it 'pops elements in reverse order' do 
+    subject.push element
+    subject.push :element_2
+    expect(subject.pop).to eq :element_2
+    expect(subject.pop).to eq element
   end
 end
