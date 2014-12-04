899bdd597a308f1046abb12775f858bc2fbdce37
Jan Grodowski <jgrodowski@gmail.com>
2014-12-04 11:01:17 +0100

    master branch ready

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,17 +1,3 @@
 class Stack  
-  def initialize
-    @elements = []
-  end
-  
-  def size 
-    @elements.size
-  end
-  
-  def push(element)
-    @elements << element
-  end
-  
-  def pop
-    @elements.pop
-  end
+
 end
